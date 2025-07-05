ad3e0360ec143881f42b221a2c6101025ee05eaa
kal <kalyano@spacelift.io>
2025-07-05 15:10:35 -0400

    Update outputs.tf

--- stacks/opentofu/outputs.tf
+++ stacks/opentofu/outputs.tf
@@ -5,3 +5,11 @@ output "ec2_ip" {
 output "s3_bucket" {
   value = var.minecraft_s3_bucket != null ? var.minecraft_s3_bucket : aws_s3_bucket.minecraft_saves[0].bucket
 }
+
+output "motd" {
+  value = var.motd
+}
+
+output "max_players" {
+  value = var.max_players
+}
