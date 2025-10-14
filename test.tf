2a599e400332eb8a994657007bdcdbe099115e3d
kal <kalyano@spacelift.io>
2025-10-14 16:46:27 -0400

    Create test.tf

---
+++ test.tf
@@ -0,0 +1,18 @@
+# main.tf
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "s3-bucket" {
+  bucket = "kals-bucket-123321"
+}
